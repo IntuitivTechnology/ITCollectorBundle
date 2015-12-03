5743f635d8a988ac9375ff19ef3e3a979df94562
Raphaël Amourette <ramourette@intuitiv.fr>
2015-12-03 10:56:19 +0100

    added config parameters optional

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,6 +23,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
                 ->arrayNode('git')
+                    ->canBeEnabled()
                     ->children()
                         ->scalarNode('branch')->defaultValue('master')->end()
                         
